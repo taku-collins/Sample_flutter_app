ade97b202fc6698a259a33122ffba5a65b99cdd4
taku-collins <ctaku@yahoo.com>
2020-09-15 23:28:41 +0100

    added the Tab bar and Controller

--- lib/pages/login/LoginPage.dart
+++ lib/pages/login/LoginPage.dart
@@ -6,58 +6,98 @@ import 'package:innovative_farming/data/databasehelper.dart';
 import 'package:innovative_farming/models/user.dart';
 import 'package:innovative_farming/pages/login/login.dart';
 
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> implements LoginPageContract {
-
   BuildContext _ctx;
   bool _isLoading;
   final formKey = new GlobalKey<FormState>();
   final scaffoldKey = new GlobalKey<ScaffoldState>();
 
-  String _username,_password;
+  String _username, _password;
 
   LoginPagePresenter _presenter;
 
-
-  _LoginPageState(){
+  _LoginPageState() {
     _presenter = new LoginPagePresenter(this);
   }
-void _submit(){
+  void _submit() {
     final form = formKey.currentState;
-
-    if(form.validate()) {
+    if (form.validate()) {
       setState(() {
         _isLoading = true;
         form.save();
         _presenter.doLogin(_username, _password);
-
       });
     }
-}
+  }
 
-  void _showSnackBar(String text){
-  scaffoldKey.currentState.showSnackBar(
-    new SnackBar(
+  void _showSnackBar(String text) {
+    scaffoldKey.currentState.showSnackBar(new SnackBar(
       content: new Text(text),
-    )
-  );
-}
-
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     _ctx = context;
     var loginBtn = new RaisedButton(
-      onPressed: _submit,
+      //onPressed: _submit,
+      onPressed: onmoceLogin,
       child: new Text("Login"),
       color: Colors.green,
     );
-    var loginform = new Column(
+
+
+    var loginform = Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          Image.asset('assets/images/land.png'),
+          new Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: new TextFormField(
+              onSaved: (val) => _username = val,
+              decoration: new InputDecoration(labelText: "Name"),
+            ),
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: new TextFormField(
+              onSaved: (val) => _password = val,
+              decoration: new InputDecoration(labelText: "Password"),
+            ),
+          ),
+          /*               ButtonBar(
+                  alignment: MainAxisAlignment.start,
+                  children: [
+                    FlatButton(
+                      textColor: const Color(0xFF6200EE),
+                      onPressed: () {
+                        // Perform some action
+                      },
+                      child: const Text('ACTION 1'),
+                    ),
+                    FlatButton(
+                      textColor: const Color(0xFF6200EE),
+                      onPressed: () {
+                        // Perform some action
+                      },
+                      child: const Text('ACTION 2'),
+                    ),
+                  ],
+                ),*/
+
+
+          loginBtn
+        ],
+      ),
+    );
+
+    /*new Column(
 
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
@@ -85,24 +125,26 @@ void _submit(){
       ],
 
     );
-
+*/
     return new Scaffold(
       appBar: new AppBar(
         leading: Icon(Icons.menu),
         title: new Text("Login64"),
         actions: [
-          Icon(Icons.favorite),
+          Icon(Icons.business),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: Icon(Icons.search),
           ),
           Icon(Icons.more_vert),
         ],
-      key: scaffoldKey,
-        ),
+        key: scaffoldKey,
+      ),
       key: scaffoldKey,
       body: new Container(
-        child: new Center(child: loginform,),
+        child: new Center(
+          child: loginform,
+        ),
       ),
     );
   }
@@ -111,16 +153,16 @@ void _submit(){
   void onLoginError(String error) {
     // TODO: implement onLoginError
     _showSnackBar(error);
-    setState(()  {
+    setState(() {
       _isLoading = false;
     });
   }
 
   @override
-  void onLoginSuccess(User user) async{
+  void onLoginSuccess(User user) async {
     // TODO: implement onLoginSuccess
     _showSnackBar(user.toString());
-    setState(()  {
+    setState(() {
       _isLoading = false;
     });
     var db = new DataBaseHelper();
@@ -129,6 +171,9 @@ void _submit(){
   }
 
 
+  @override
+  void onmoceLogin ( ) async {
 
-
+    Navigator.of(context).pushNamed("/home");
+  }
 }
